8b01fad8067d347d3b3fe15bbdf6bce629c24b37
johncube78 <johncube78@localhost>
2011-04-24 14:52:55 +0200

    Adding admin Stats.

---
+++ handlers/module/admin/AdminStats.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+##
+# (C) Copyright 2011 Jose Blanco <jose.blanco[a]vikuit.com>
+# 
+# This file is part of "vikuit".
+# 
+# "vikuit" is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# "vikuit" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with "vikuit".  If not, see <http://www.gnu.org/licenses/>.
+## 
+
+from handlers.AuthenticatedHandler import *
+
+class AdminStats(AuthenticatedHandler):
+
+	def execute(self):
+		method = self.request.method
+		user = self.values['user']
+		self.values['tab'] = '/module/admin.stats'
+
+		if user.rol != 'admin':
+			self.forbidden()
+			return
+		self.render('templates/module/admin/admin-stats.html')
+		 
